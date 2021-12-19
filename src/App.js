a82abd4741ec693d535ae9e77d9c15d973a4554e
bleskova <bleskova.kate@gmail.com>
2021-12-19 14:15:54 +0200

    twettrwet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import Header from './components/Header/Header'
 import './App.css';
 import Vlad from "./components/Vlad/index";
 
 function App() {
   return (
     <div className="App">
+      <Header />
     <footer>
       <Vlad/>
     </footer>
+      
     </div>
   );
 }
