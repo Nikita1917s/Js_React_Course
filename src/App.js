903659890693063eceb6f51853993ce0531d6497
Tulupov Nikita <nekit1917@gmail.com>
2021-12-30 01:40:27 +0200

    upload h/w

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from './components/Header/Header';
+import './App.css';
+import Vlad from "./components/Vlad/index";
+import ItemsList from './components/ItemsList/ItemsList';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+    <ItemsList />
+    <footer>
+      <Vlad/>
+    </footer>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css'
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+            <div className="header-container flex">
+                <div className="header-container-item "><img src="" alt=""/>1</div>
+                <div className="header-container-item"><a href="#">STORY</a></div>
+                <div className="header-container-item"><a href="#">FRESH</a></div>
+                <div className="header-container-item "><a href="#">FRESH</a></div>
+                <div className="header-container-item"><a href="#">FRESH</a></div>
+            </div>
+            <div>
+                <div className="header-pic"></div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
